990c3c26dcedc69a37ac950495bc6df7ee5a4b30
Nadeem Shaikh <nadeemns1104@gmail.com>
2023-06-06 19:07:17 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/farm_flow_sales/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.farm_flow_sales
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/resources/routes/route_name.dart
@@ -0,0 +1,15 @@
+class RouteName {
+  //login & registration
+  static const String splashScreen = '/';
+  static const String loginScreen = '/loginScreen';
+  static const String forgotPassword = '/forgotPassword';
+  static const String verifyNumber = '/verifyNumber';
+  static const String resetPassword = '/ResetPassword';
+
+// home
+  static const String sideMenu = '/sideMenu';
+
+  // side menu
+  static const String contact = '/contact';
+  static const String faqs = '/faq';
+}
