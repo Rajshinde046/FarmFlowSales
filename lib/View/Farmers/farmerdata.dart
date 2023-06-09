b5e604ea94a3240ae0446b3a05c9cf5505a7a54c
Shubhamshirva <shubhamshirva@gmail.com>
2023-06-09 17:38:46 +0530

    created screens for warehouse,farmer,products and connect farmer

---
+++ lib/View/Farmers/farmerdata.dart
@@ -0,0 +1,42 @@
+List farmerdata = [
+  {
+    "image": "assets/images/contactperson2.png",
+    "tittle": "John O Brien",
+    "number": "0873457549",
+  },
+  {
+    "image": "assets/images/Contactoerson.png",
+    "tittle": "Seamus Lynch",
+    "number": "0861238765",
+  },
+  {
+    "image": "assets/images/farmer3.png",
+    "tittle": "Liam Mcarthy",
+    "number": "08311198761",
+  },
+  {
+    "image": "assets/images/farmer4.png",
+    "tittle": "Rory Ryan",
+    "number": "0864477897",
+  },
+  {
+    "image": "assets/images/farmer5.png",
+    "tittle": "James Fogarty",
+    "number": "0873666234",
+  },
+  {
+    "image": "assets/images/farmer3.png",
+    "tittle": "Helen Coffey",
+    "number": "0875841212",
+  },
+  {
+    "image": "assets/images/farmer6.png",
+    "tittle": "William Ryan",
+    "number": "0831123459",
+  },
+  {
+    "image": "assets/images/farmer7.png",
+    "tittle": "James Seymour",
+    "number": "0870076756",
+  },
+];
