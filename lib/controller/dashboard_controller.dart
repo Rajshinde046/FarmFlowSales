33054224229d574dc319f39da52ff5ed08173a3d
jayesh <jayeshkjain6@gmail.com>
2023-10-31 19:36:35 +0530

    dashboard api and weather api

--- lib/controller/dashboard_controller.dart
+++ lib/controller/dashboard_controller.dart
@@ -1,3 +1,17 @@
 import 'package:get/get.dart';
 
-class DashboardController extends GetxController {}
+import '../Model/dashboardModel/dashboard_model.dart';
+
+//import '../models/dashboardModel.dart';
+
+class DashboardController extends GetxController {
+  RxString permissionStatus = "".obs;
+  RxString tempValue = "00.0".obs;
+  RxString humidityValue = "0".obs;
+  RxString windValue = "00.0".obs;
+  RxString locationText = "Unknown".obs;
+  RxBool isLocationFetching = false.obs;
+  RxString weatherCondition = "".obs;
+  RxBool isDashboardApiLoading = false.obs;
+  DashboardModel dashboardModel = DashboardModel();
+}
